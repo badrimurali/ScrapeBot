53fbb5c5cf5048978e392e5caff32caf284b43b9
badrimurali <badriitindia@gmail.com>
2017-09-12 22:02:00 +0530

    make es lint happy

--- src/init.py
+++ src/init.py
@@ -1,29 +1,25 @@
+"""this is a scrapebot for scraping any website"""
 from bs4 import BeautifulSoup
 from TagParser import getAll
 import requests
-import urllib
-import os
 
-url = "http://www.goal.com/en"
-r = requests.get(url);
-htmlData = r.text
-soup = BeautifulSoup(htmlData, "html.parser")
-imgs = getAll(soup, "img")
-paragraphs = getAll(soup, "p")
-divs = getAll(soup, "div")
-for img in imgs:
-	imgUrl = img["src"]
-	print(imgUrl)
-	# urllib.urlretrieve(imgUrl, os.path.basename(imgUrl))
+URL = "http://www.goal.com/en"
+R = requests.get(URL)
+HTML_DATA = R.text
+SOUP = BeautifulSoup(HTML_DATA, "html.parser")
+IMGS = getAll(SOUP, "img")
+PARA = getAll(SOUP, "p")
+DIVS = getAll(SOUP, "div")
+for img in IMGS:
+    imgUrl = img["src"]
+    print imgUrl
 
-for p in paragraphs:
-	for child in p.children:
-		print(child)
-		
-for div in divs:
-	print(len(list(div.children)))
-	divList = list(div.children)
-	for i in range(len(divList)):
-		print("i", i, divList[i])
+for p in PARA:
+    for child in p.children:
+        print child
 
-
+for div in DIVS:
+    print len(list(div.children))
+    divList = list(div.children)
+    for count, divItem in enumerate(divList):
+        print("i", count, divItem)
